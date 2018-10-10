8999b9c7eb3299eba999b94ccfba3901a4b109a0
pri5m <ilex.laurel.smith@gmail.com>
2018-10-10 11:50:42 +0100

    first commit

---
+++ src/main/java/com/nsa/c1712480/tacocloud/TacocloudApplication.java
@@ -0,0 +1,12 @@
+package com.nsa.c1712480.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacocloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacocloudApplication.class, args);
+    }
+}
---
+++ src/main/java/com/nsa/c1712480/tacocloud/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.nsa.c1712480.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
